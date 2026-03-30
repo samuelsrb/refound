a5a479a5197365f1979c8ba94c1b0c7d4c4df6f9
samuelsrb <ssrbarroso@gmail.com>
2026-03-30 18:17:26 -0300

    object with new expenses in JS

--- script.js
+++ script.js
@@ -25,4 +25,15 @@ function formatCurrencyBRL(value) {
 
 form.onsubmit = (e) => {
   e.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  console.log(newExpense);
 };
