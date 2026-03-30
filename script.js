61250072b3cef610d60d50c946f4f661a651a990
samuelsrb <ssrbarroso@gmail.com>
2026-03-30 17:56:05 -0300

    format input value to BRL in JS

---
+++ script.js
@@ -0,0 +1,21 @@
+// Elementos do formulário
+const amount = document.getElementById("amount");
+
+// Captura o evento de input para formatar o valor
+amount.oninput = () => {
+  let value = amount.value.replace(/\D/g, "");
+
+  value = Number(value) / 100;
+
+  amount.value = formatCurrencyBRL(value);
+};
+
+function formatCurrencyBRL(value) {
+  // Formata o valor no padrão BRL
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  return value;
+}
