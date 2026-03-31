900e32146657a6ca5ff222cf179b6c85a058330b
samuelsrb <ssrbarroso@gmail.com>
2026-03-30 21:23:49 -0300

    Add the icons to the list of items

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const amount = document.getElementById("amount");
 const expense = document.getElementById("expense");
 const category = document.getElementById("category");
 
+// Seleciona os elementos da lista.
+const expenseList = document.querySelector("ul");
+
 // Captura o evento de input para formatar o valor
 amount.oninput = () => {
   let value = amount.value.replace(/\D/g, "");
@@ -44,6 +47,17 @@ function expenseAdd(newExpense) {
     // Cria o elemento de li para adicionar o item (li) na lista (ul).
     const expenseItem = document.createElement("li");
     expenseItem.classList.add("expense");
+
+    // Cria o ícone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon);
+
+    // Adiciona o item na lista
+    expenseList.append(expenseItem);
   } catch (error) {
     alert("Não foi possível atualizar a lista de despesas.");
     console.log(error);
