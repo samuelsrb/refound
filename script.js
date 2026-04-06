e51166aef1050f53ba52f0bd22d2b4d41055fc27
samuelsrb <ssrbarroso@gmail.com>
2026-04-06 14:19:30 -0300

    Creates the remove icon

--- script.js
+++ script.js
@@ -73,8 +73,15 @@ function expenseAdd(newExpense) {
     expenseAmount.classList.add("expense-amount");
     expenseAmount.innerHTML = `<smal>R$</smal>${newExpense.amount.toUpperCase().replace("R$", "")}`;
 
+    // Cria o icone de remover
+
+    const removeIcon = document.createElement("img");
+    removeIcon.classList.add("remove-icon");
+    removeIcon.setAttribute("src", "img/remove.svg");
+    removeIcon.setAttribute("alt", "remover");
+
     // Adiciona as informações no item
-    expenseItem.append(expenseIcon, expenseInfo, expenseAmount);
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon);
 
     // Adiciona o item na lista
     expenseList.append(expenseItem);
